50d6c23779f780aa7290e93d409dea901026e390
khandakarfahimahmed <fahim363501@gmail.com>
2024-02-12 13:59:41 +0600

    first commit

---
+++ src/app/housing-location/housing-location.component.ts
@@ -0,0 +1,28 @@
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HousingLocation } from '../housinglocation';
+import { RouterLink, RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-housing-location',
+  standalone: true,
+  imports: [RouterLink, RouterOutlet],
+  template: `
+    <section class="listing">
+      <img
+        class="listing-photo"
+        [src]="housingLocation.photo"
+        alt="Exterior photo of {{ housingLocation.name }}"
+      />
+      <h2 class="listing-heading">{{ housingLocation.name }}</h2>
+      <p class="listing-location">
+        {{ housingLocation.city }}, {{ housingLocation.state }}
+      </p>
+      <a [routerLink]="['/details', housingLocation.id]">Learn More</a>
+    </section>
+  `,
+  styleUrl: './housing-location.component.css',
+})
+export class HousingLocationComponent {
+  @Input() housingLocation!: HousingLocation; //! is the non-null assertion operator and it tells the TypeScript compiler that the value of this property won't be null or undefined.
+}
